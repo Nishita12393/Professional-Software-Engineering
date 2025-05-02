a0f96bb7681054f483491d5cb6b0678bd01d59f6
Nishita12393 <nishitathapa1111@gmail.com>
2025-05-03 09:56:50 +1200

    csv read file

--- PyThon/27063325-nishita/W3-5/factorial_class.py
+++ PyThon/27063325-nishita/W3-5/factorial_class.py
@@ -1,15 +1,23 @@
 class FactorialOperations:
-    def factorial(self, x):
-        if x < 0:
-            return "Cannot be factorized"
+    def __init__(self, x: int|str|list[int], y, z):
+        self.x = x
+        self.y = y
+        if(self.y < 0):
+           self.y = 0
+
+    def factorial(this) -> int:
+        if this.x < 0:
+            raise ValueError("Invalid number. Number must be greater than zero.") 
         else:
             result = 1
-            for i in range(1, x + 1):
+            for i in range(1, this.x + 1):
                 print("Before multiplying:", result)
                 result *= i
                 print("After multiplying:", result)
             return result
 
+
 # Using the class
-fact_op = FactorialOperations()
-print(fact_op.factorial(5))
+fact_op = FactorialOperations(1, -6, 7)
+h = fact_op.factorial()
+print(h)
